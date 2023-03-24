fad99b650bb2d0a3095e457e2d8c99722f361be3
KevinBacklund <kebacklund@gmail.com>
2023-03-24 11:41:25 +0100

    translate method

--- Program.cs
+++ Program.cs
@@ -50,28 +50,7 @@
                 }
                 else if (command == "translate")
                 {
-                    if (argument.Length == 2)
-                    {
-                        foreach(SweEngGloss gloss in dictionary)
-                        {
-                            if (gloss.word_swe == argument[1])
-                                Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
-                            if (gloss.word_eng == argument[1])
-                                Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
-                        }
-                    }
-                    else if (argument.Length == 1)
-                    {
-                        Console.WriteLine("Write word to be translated: ");
-                        string word_to_be_translated = Console.ReadLine();
-                        foreach (SweEngGloss gloss in dictionary)
-                        {
-                            if (gloss.word_swe == word_to_be_translated)
-                                Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
-                            if (gloss.word_eng == word_to_be_translated)
-                                Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
-                        }
-                    }
+                    translate(argument);
                 }
                 else
                 {
@@ -79,6 +58,32 @@
                 }
             }
             while (true);
+        }
+
+        private static void translate(string[] argument)
+        {
+            if (argument.Length == 2)
+            {
+                foreach (SweEngGloss gloss in dictionary)
+                {
+                    if (gloss.word_swe == argument[1])
+                        Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
+                    if (gloss.word_eng == argument[1])
+                        Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
+                }
+            }
+            else if (argument.Length == 1)
+            {
+                Console.WriteLine("Write word to be translated: ");
+                string word_to_be_translated = Console.ReadLine();
+                foreach (SweEngGloss gloss in dictionary)
+                {
+                    if (gloss.word_swe == word_to_be_translated)
+                        Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
+                    if (gloss.word_eng == word_to_be_translated)
+                        Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
+                }
+            }
         }
 
         private static void delete(string[] argument)
